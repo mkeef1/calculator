73826848601782585dbbd339aa5c27bced23f057
mkeef1 <mkeef1@gmail.com>
2014-07-17 15:32:12 -0500

    calculator app

---
+++ app/models/calc.js
@@ -0,0 +1,48 @@
+'use strict';
+
+function Calc(){}
+
+Calc.add = function(a,b){
+  return a + b;
+};
+
+Calc.sub = function(a,b){
+  return a - b;
+};
+
+Calc.distance = function(a,b){
+  var xDiff = (a.x - b.x);
+  var yDiff = (a.y - b.y);  
+  var result =   Math.sqrt(Math.pow(xDiff,2) + (Math.pow(yDiff,2)));
+  return result;
+};
+
+Calc.line = function(a,b){
+  var xDiff = (a.x - b.x);
+  var yDiff = (a.y - b.y);  
+  var slope = (yDiff/xDiff);
+  var yInt = (a.y - (a.x * slope));
+  if(yInt >= 0){
+  return ('y = ' + slope.toFixed(1) + 'x + ' + Math.abs(yInt).toFixed(1));
+}else{
+  return ('y = ' + slope.toFixed(1) + 'x - ' + Math.abs(yInt).toFixed(1));
+  }
+};
+
+Calc.trip = function(a){
+  var tripTotal = 0;
+  for (var i = 0; (i + 1) < a.length; i++){
+    var step = Calc.distance(a[i],a[i+1]);
+    tripTotal += step;
+  } 
+  return tripTotal;
+};
+
+Calc.mean = function(a){
+  var total = 0;
+  for ( var i = 0; i < a.length; i++){
+    total += a[i];
+  }
+  return total/a.length;
+};
+module.exports = Calc;
---
+++ test/unit/calc.js
@@ -0,0 +1,48 @@
+/* global describe, it*/
+'use strict';
+
+var expect = require('chai').expect;
+var Calc = require('../../app/models/calc');
+
+describe('Calc', function(){
+  describe('.add', function(){
+    it('should sum two numbers', function(){
+      var sum = Calc.add(2,3);
+      expect(sum).to.equal(5);
+    });
+  });
+  describe('.sub', function(){
+    it('should subtract two numbers', function(){
+      var diff = Calc.sub(5,3);
+      expect(diff).to.equal(2);
+    });
+  });
+  describe('.distance', function(){
+    it('should find distance between 2 points in 2d', function(){
+      var distance = Calc.distance({x:4,y:7},{x:8,y:10});
+      expect(distance).to.equal(5);
+      });
+    });
+  describe('.line', function(){
+    it('should find the slope of a line', function(){
+      var line = Calc.line({x:3,y:2},{x:7,y:4});
+      expect(line).to.equal('y = 0.5x + 0.5');
+    });
+    it('should find the slope of a line with negative slope and y int', function(){
+      var line = Calc.line({x:1,y:-2},{x:2,y:-3});
+      expect(line).to.equal('y = -1.0x - 1.0');
+    });
+  });
+   describe('.trip', function(){
+    it('should give total distance to travel an array of points', function(){
+      var trip = Calc.trip([{x:1,y:1},{x:5,y:-2},{x:10,y:-2}]);
+      expect(trip).to.equal(10);
+    });
+   });
+  describe('.mean', function(){
+    it('should return the mean of the array', function(){
+      var mean = Calc.mean([4, 5, 7, 8]);
+      expect(mean).to.equal(6);
+    });
+  });
+});
